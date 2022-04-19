af345f1e0368dc88162b856e991eb75796c0fb67
hammackjg <hammackjg@g.crookcountyschools.org>
2022-04-18 20:04:10 -0700

    modified index.html

--- forms.js
+++ forms.js
@@ -3,44 +3,38 @@
 const form = document.getElementsByTagName('*')[0];
 const label = document.createElement('label');
 const input = document.createElement('input');
-const submit = document.createElement('input');
+const submit = document.createElement('submit');
 const fieldset = document.getElementById('first');
 const content = document.createElement('textarea');
 
-label.textContent = 'Topic';
-input.type ="text";
-input.id = "title";
-input.name = "input"
-content.id = "info"
-content.name = "content"
-content.rows ="5";
-content.cols ="30";
-submit.type = "submit";
 
+label.textContent = "Journal Entry:"
+input.type = 'text';
+input.id = "title";
+input.name = 'input';
+content.id = 'info';
+content.name = "content";
+submit.type = "submit";
+content.rows = "10";
+content.cols = "30";
 
 fieldset.appendChild(label);
 fieldset.appendChild(input);
-fieldset.appendChild(submit);
 fieldset.appendChild(content);
-
+fieldset.appendChild(submit);
 
 form.addEventListener('submit', handleSubmit);
-
 function handleSubmit(event){
     event.preventDefault();
-    const title = event.target.value;
-    const content = event.target.content;
+    var title = event.target.value;
+    var content = event.target.content.value;
 
-    const h1 = document.createElement('h1');
-    const p = document.createElement('p');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
 
     h1.textContent = title;
     p.textContent = content;
 
     const formContent = document.getElementById('render');
     
-    formContent.appendChild(h1);
-    formContent.appendChild(p);
-
 }
-
